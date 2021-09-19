41aaad8724304fb3eba4b4149dd5e34aec91a8f0
HE140946-Bùi Hoàng Phong <90886996+t18maimaimottinhyeu@users.noreply.github.com>
2021-09-19 18:39:54 +0700

    Update BlogDAOitf.java

--- SWP391-Project/src/java/dao/BlogDAOitf.java
+++ SWP391-Project/src/java/dao/BlogDAOitf.java
@@ -3,12 +3,44 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+import bean.Blog;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.sql.Connection;
+import util.DBConnection;
+
 package dao;
 
 /**
  *
- * @author Viettech88.vn
+ * @author phong
  */
 public interface BlogDAOitf {
+    private Connection con;
+    private PreparedStatement ps;
+    private ResultSet rs;
+
+    private void closeConnection() throws SQLException {
+        if (con != null) {
+            con.close();
+        }
+        if (rs != null) {
+            rs.close();
+        }
+        if (ps != null) {
+            ps.close();
+        }
+    }
     
+    public ArrayList<Blog> getAllBlog() throws Exception {
+    String sql = "SELECT * FROM Blog";
+    ArrayList<Blog> list = new ArrayList<>();
+    DBConnection db = new DBConnection();
+    con = db.getConnection();
+    if (con != null) {
+                    
+    }
+    return null;
 }
