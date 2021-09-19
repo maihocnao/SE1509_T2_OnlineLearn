8d3f79c6889845ad5ec685c13cedcb6cb1a742f4
HE140946-Bùi Hoàng Phong <90886996+t18maimaimottinhyeu@users.noreply.github.com>
2021-09-19 18:09:13 +0700

    Update AnswerDAOitf.java

--- SWP391-Project/src/java/dao/AnswerDAOitf.java
+++ SWP391-Project/src/java/dao/AnswerDAOitf.java
@@ -3,13 +3,52 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+import bean.Answer;
+import java.sql.PreparedStatement;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.sql.Connection;
+import javax.jms.JMSException;
+import util.DBConnection;
+
 package dao;
 
 /**
  *
- * @author Viettech88.vn
+ * @author phong
  */
 public interface AnswerDAOitf {
-    aaaaa
+   private Connection con;
+    private PreparedStatement ps;
+    private ResultSet rs;
+
+    public AnswerDAO() {
+    }
+
+    private void closeConnection() throws SQLException, JMSException {
+        if (con != null) {
+            con.close();
+        }
+        if (rs != null) {
+            rs.close();
+        }
+        if (ps != null) {
+            ps.close();
+        }
+    }
+    public ArrayList<Answer> getAllAnswer() throws SQLException, JMSException {
+        ArrayList<Answer> answerList = new ArrayList<>();
+        String sql = "select answerID, answer.questionID, result, answer.content from answer";
+        try {
+            DBConnection db = new DBConnection();
+            con = db.getConnection();
+            if (con != null) {
+                ps = con.prepareStatement(sql);
+                rs = ps.executeQuery();
+                while (rs.next()) {
+                    
+    }
     
 }
