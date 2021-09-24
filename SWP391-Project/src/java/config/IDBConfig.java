401d648f86538cf3b64e130d813109d004fa5795
HE140946-Bùi Hoàng Phong <90886996+t18maimaimottinhyeu@users.noreply.github.com>
2021-09-24 17:23:27 +0700

    Add files via upload

---
+++ SWP391-Project/src/java/config/IDBConfig.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package config;
+
+/**
+ *
+ * @author phuon
+ */
+public interface IDBConfig {
+    public static final String HOSTNAME = "LAPTOP-Q0R87QMU\\SQLEXPRESS";
+    public static final String PORT = "1433";
+    public static final String DATABASENAME = "SWP";
+    public static final String INTEGRATEDSECURITY = "false";
+    public static final String USERNAME = "sa";
+    public static final String PASSWORD = "sa";
+}
