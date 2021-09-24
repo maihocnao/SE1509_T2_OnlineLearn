4a201d7ea121ca6afc8ebcfbc15966f5cb19ed4e
HE140946-Bùi Hoàng Phong <90886996+t18maimaimottinhyeu@users.noreply.github.com>
2021-09-24 13:38:20 +0700

    Update Login.java

--- SWP391-Project/src/java/dao/impl/Login.java
+++ SWP391-Project/src/java/dao/impl/Login.java
@@ -4,36 +4,28 @@
  * and open the template in the editor.
  */
 package dao.impl;
+
 import bean.User;
-import context.DBConnect;
-import dao.itf.UserDAO ;
+import context.MSSQLConnection;
+import static context.MSSQLConnection.getConnection;
+import dao.itf.UserDAO;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 /**
  *
  * @author Viettech88.vn
  */
-public class Login implements UserDAO{
+public class Login implements UserDAO {
+
     private Connection con;
     private PreparedStatement ps;
     private ResultSet rs;
 
-    private void closeConnection() throws SQLException {
-        if (con != null) {
-            con.close();
-        }
-        if (rs != null) {
-            rs.close();
-        }
-        if (ps != null) {
-            ps.close();
-        }
-    }
-   
     /**
      *
      * @param email
@@ -42,69 +34,38 @@ public class Login implements UserDAO{
      * @throws Exception
      */
     @Override
-        public User checkLogin(String email, String password) {
+    public User checkLogin(String email, String password) {
         User user = null;
-        String sql = "select * from User where Email=? AND Password=?";
+        String sql = "select * FROM [SWP].[dbo].[User] where email=? AND password=?";
+        
         try {
-            DBConnect db = new DBConnect();
-            con = db.getConnection();
-            if (con != null) {
-                ps = con.prepareStatement(sql);
-                ps.setString(1, email);
-                ps.setString(2, password);
-                rs = ps.executeQuery();
-                if (rs.next()) {
-                    String roleID = rs.getString("RoleID");
-                    int settingID = rs.getInt("SettingID");
-                    int userID = Integer.parseInt(rs.getString("UserID"));
-                    email = rs.getString("Email");
-                    String gender = rs.getString("Gender");
-                    String fullname = rs.getString("Fullname");
-                    String phone = rs.getString("Phone");
-                    user = new User(userID,settingID, roleID, email, password, gender, fullname, phone);
-                }
+            con = MSSQLConnection.getConnection();
+            ps = con.prepareStatement(sql) ; 
+            ps.setString(1, email);
+            ps.setString(2, password);
+            rs = ps.executeQuery();
+//            int UserID, int SettingID, String RoleID, String Email, String Password, String Gender, String Fullname, String Phone
+            if (rs.next()) {
+                return new User(rs.getInt("UserID"), rs.getInt("SettingID")
+                ,rs.getString("RoleID"),rs.getString("Email"),rs.getString("Password")
+                ,rs.getString("Gender"),rs.getString("Fullname"),rs.getString("Phone"));
             }
+            
         } catch (Exception e) {
-            e.printStackTrace();
+            System.out.println(e);
         } finally {
-            try {
-                closeConnection();
-            } catch (SQLException ex) {
-                Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
-            }
+            MSSQLConnection.closeResultSet(rs);
+            MSSQLConnection.closePreparedStatement(ps);
+            MSSQLConnection.closeConnection(con);
         }
-        return user ;
+        return user;
     }
-//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-
-//    @Override
-//    public void checkLogin() {
-//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-//    }
-//
-//    @Override
-//    public boolean checkAccountExist() {
-//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-//    }
-//
-//    @Override
-//    public void changePassword() {
-//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-//    }
-//
-//    @Override
-//    public void changName() {
-//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-//    }
-//
-//    @Override
-//    public void resetPassword() {
-//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-//    }
-//    
-
+    public static void main(String[] args) {
+        Login login = new Login();
+        login.checkLogin("admin@gmail.com","123") ; 
+    }
     @Override
-    public boolean checkAccountExist(String Email) {
+    public boolean checkAccountExist() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
@@ -119,7 +80,8 @@ public class Login implements UserDAO{
     }
 
     @Override
-    public void resetPassword(String email) {
+    public void resetPassword() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
+
 }
