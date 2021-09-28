e260228257b5a5744d4a1e894c45062a80c09a7a
maihocnao <85759025+maihocnao@users.noreply.github.com>
2021-09-28 12:46:37 +0700

    Update Subject.java

--- SWP391-Project/src/java/bean/Subject.java
+++ SWP391-Project/src/java/bean/Subject.java
@@ -10,67 +10,87 @@ package bean;
  * @author Phong
  */
 public class Subject {
-    int subjectID ; 
+    String subjectID ; 
     int categoryID ; 
     String thumbnail;
     String name; 
     String Description; 
+    String status;
+    String featured;
 
     public Subject() {
     }
 
-    public Subject(int subjectID, int categoryID, String thumbnail, String name, String Description) {
+    public Subject(String subjectID, int categoryID, String thumbnail, String name, String Description, String status, String featured) {
         this.subjectID = subjectID;
         this.categoryID = categoryID;
         this.thumbnail = thumbnail;
         this.name = name;
         this.Description = Description;
+        this.status = status;
+        this.featured = featured;
     }
 
-    public int getSubjectID() {
+    public String getSubjectID() {
         return subjectID;
+    }
+
+    public void setSubjectID(String subjectID) {
+        this.subjectID = subjectID;
     }
 
     public int getCategoryID() {
         return categoryID;
     }
 
-    public String getThumbnail() {
-        return thumbnail;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getDescription() {
-        return Description;
-    }
-
-    public void setSubjectID(int subjectID) {
-        this.subjectID = subjectID;
-    }
-
     public void setCategoryID(int categoryID) {
         this.categoryID = categoryID;
+    }
+
+    public String getThumbnail() {
+        return thumbnail;
     }
 
     public void setThumbnail(String thumbnail) {
         this.thumbnail = thumbnail;
     }
 
+    public String getName() {
+        return name;
+    }
+
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getDescription() {
+        return Description;
     }
 
     public void setDescription(String Description) {
         this.Description = Description;
     }
 
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getFeatured() {
+        return featured;
+    }
+
+    public void setFeatured(String featured) {
+        this.featured = featured;
+    }
+
     @Override
     public String toString() {
-        return "Subject{" + "subjectID=" + subjectID + ", categoryID=" + categoryID + ", thumbnail=" + thumbnail + ", name=" + name + ", Description=" + Description + '}';
+        return "Subject{" + "subjectID=" + subjectID + ", categoryID=" + categoryID + ", thumbnail=" + thumbnail + ", name=" + name + ", Description=" + Description + ", status=" + status + ", featured=" + featured + '}';
     }
-    
+
     
 }
