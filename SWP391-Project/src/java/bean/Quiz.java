5fb172d4eb2921558aac798280f2dde63ae61c84
maihocnao <85759025+maihocnao@users.noreply.github.com>
2021-09-28 12:48:03 +0700

    Update Quiz.java

--- SWP391-Project/src/java/bean/Quiz.java
+++ SWP391-Project/src/java/bean/Quiz.java
@@ -12,26 +12,23 @@ package bean;
 public class Quiz {
     int quizID ; 
     int lessonID ; 
-    int numOfQuestion; 
-    int duration;
-    int subjectID; 
-    float passRate; 
     String quizName; 
     String level; 
+    int numOfQuestion; 
+    int duration;//minute
+    float passRate;  
     String quizType; 
-
     public Quiz() {
     }
 
-    public Quiz(int quizID, int lessonID, int numOfQuestion, int duration, int subjectID, float passRate, String quizName, String level, String quizType) {
+    public Quiz(int quizID, int lessonID, String quizName, String level, int numOfQuestion, int duration, float passRate, String quizType) {
         this.quizID = quizID;
         this.lessonID = lessonID;
-        this.numOfQuestion = numOfQuestion;
-        this.duration = duration;
-        this.subjectID = subjectID;
-        this.passRate = passRate;
         this.quizName = quizName;
         this.level = level;
+        this.numOfQuestion = numOfQuestion;
+        this.duration = duration;
+        this.passRate = passRate;
         this.quizType = quizType;
     }
 
@@ -39,68 +36,60 @@ public class Quiz {
         return quizID;
     }
 
-    public int getLessonID() {
-        return lessonID;
-    }
-
-    public int getNumOfQuestion() {
-        return numOfQuestion;
-    }
-
-    public int getDuration() {
-        return duration;
-    }
-
-    public int getSubjectID() {
-        return subjectID;
-    }
-
-    public float getPassRate() {
-        return passRate;
-    }
-
-    public String getQuizName() {
-        return quizName;
-    }
-
-    public String getLevel() {
-        return level;
-    }
-
-    public String getQuizType() {
-        return quizType;
-    }
-
     public void setQuizID(int quizID) {
         this.quizID = quizID;
+    }
+
+    public int getLessonID() {
+        return lessonID;
     }
 
     public void setLessonID(int lessonID) {
         this.lessonID = lessonID;
     }
 
-    public void setNumOfQuestion(int numOfQuestion) {
-        this.numOfQuestion = numOfQuestion;
-    }
-
-    public void setDuration(int duration) {
-        this.duration = duration;
-    }
-
-    public void setSubjectID(int subjectID) {
-        this.subjectID = subjectID;
-    }
-
-    public void setPassRate(float passRate) {
-        this.passRate = passRate;
+    public String getQuizName() {
+        return quizName;
     }
 
     public void setQuizName(String quizName) {
         this.quizName = quizName;
     }
 
+    public String getLevel() {
+        return level;
+    }
+
     public void setLevel(String level) {
         this.level = level;
+    }
+
+    public int getNumOfQuestion() {
+        return numOfQuestion;
+    }
+
+    public void setNumOfQuestion(int numOfQuestion) {
+        this.numOfQuestion = numOfQuestion;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public float getPassRate() {
+        return passRate;
+    }
+
+    public void setPassRate(float passRate) {
+        this.passRate = passRate;
+    }
+
+    public String getQuizType() {
+        return quizType;
     }
 
     public void setQuizType(String quizType) {
@@ -109,8 +98,8 @@ public class Quiz {
 
     @Override
     public String toString() {
-        return "Quiz{" + "quizID=" + quizID + ", lessonID=" + lessonID + ", numOfQuestion=" + numOfQuestion + ", duration=" + duration + ", subjectID=" + subjectID + ", passRate=" + passRate + ", quizName=" + quizName + ", level=" + level + ", quizType=" + quizType + '}';
+        return "Quiz{" + "quizID=" + quizID + ", lessonID=" + lessonID + ", quizName=" + quizName + ", level=" + level + ", numOfQuestion=" + numOfQuestion + ", duration=" + duration + ", passRate=" + passRate + ", quizType=" + quizType + '}';
     }
-    
-    
+
+   
 }
