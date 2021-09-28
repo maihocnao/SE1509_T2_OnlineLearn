03a99465ddd1dd6fb465245122efb323c88505ba
maihocnao <85759025+maihocnao@users.noreply.github.com>
2021-09-28 12:47:42 +0700

    Update Question.java

--- SWP391-Project/src/java/bean/Question.java
+++ SWP391-Project/src/java/bean/Question.java
@@ -13,43 +13,53 @@ public class Question {
     int questionID ; 
     int quizID ; 
     String content ; 
+    String level;
 
     public Question() {
     }
 
-    public Question(int questionID, int quizID, String content) {
+    public Question(int questionID, int quizID, String content, String level) {
         this.questionID = questionID;
         this.quizID = quizID;
         this.content = content;
+        this.level = level;
     }
 
     public int getQuestionID() {
         return questionID;
     }
 
-    public int getQuizID() {
-        return quizID;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
     public void setQuestionID(int questionID) {
         this.questionID = questionID;
+    }
+
+    public int getQuizID() {
+        return quizID;
     }
 
     public void setQuizID(int quizID) {
         this.quizID = quizID;
     }
 
+    public String getContent() {
+        return content;
+    }
+
     public void setContent(String content) {
         this.content = content;
     }
 
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
     @Override
     public String toString() {
-        return "Question{" + "questionID=" + questionID + ", quizID=" + quizID + ", content=" + content + '}';
+        return "Question{" + "questionID=" + questionID + ", quizID=" + quizID + ", content=" + content + ", level=" + level + '}';
     }
     
     
