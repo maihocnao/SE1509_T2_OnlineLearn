3851ee70f32972c6dda94c2897ad540c39cffa99
DannyL999 <Dannylance2109@gmail.com>
2021-10-01 07:43:30 +0700

    Update BlogList.java

--- SWP391-Project/src/java/dao/impl/BlogList.java
+++ SWP391-Project/src/java/dao/impl/BlogList.java
@@ -39,15 +39,6 @@ public class BlogList extends MyDAO implements BlogDAO {
                 while (rs.next()) {
                     // query information in db
                     Blog bl = new Blog();
-//                    int blogID = rs.getInt("blogID");
-//                    int userID = rs.getInt("userID");
-//                    int categoryID = rs.getInt("categoryID");
-//                    String thumbnail = rs.getString("thumbnail");
-//                    String title = rs.getString("title");
-//                    String updatedDate = rs.getString("updatedDate");
-//                    String blogContent = rs.getString("blog content");
-//                    byte flag = rs.getByte("flag");
-//                    String status = rs.getString("status");
                     int blogID = rs.getInt(1);
                 int userID = rs.getInt(2);
                 String fullname = rs.getString(3);
@@ -126,26 +117,19 @@ public class BlogList extends MyDAO implements BlogDAO {
                 rs = ps.executeQuery();
 
                 while (rs.next()) {
-                    int blogID = rs.getInt("blogID");
-                    String fullname = rs.getString("fullname");
-                    int categoryID = rs.getInt("categoryID");
-                    String thumbnail = rs.getString("thumbnail");
-                    String title = rs.getString("title");
-                    String updatedDate = rs.getString("updatedDate");
-                    String blogDetail = rs.getString("blogDetail");
-                    int flag = rs.getInt("flag");
-                    int status = rs.getInt("status");
-                    BlogStatus blogStatus;
-                    boolean blogFlag = (flag == 1);
-                    // convert status type to enum
-                    if (status == 1) {
-                        blogStatus = BlogStatus.PUBLISH;
-                    } else {
-                        blogStatus = BlogStatus.DRAFT;
-                    }
-
-//                    Blog blog = new Blog(blogID, fullname, categoryID, thumbnail, title, updatedDate, blogDetail, blogFlag, blogStatus);
-//                    list.add(blog);
+                      Blog bl = new Blog();
+                    int blogID = rs.getInt(1);
+                int userID = rs.getInt(2);
+                String fullname = rs.getString(3);
+                int categoryID = rs.getInt(4);
+                String thumbnail = rs.getString(5);
+                String title = rs.getString(6);
+                String updatedDate = rs.getString(7);
+                String blogDetail = rs.getString(8);
+                boolean flag = rs.getBoolean(9);
+                byte status = rs.getByte(10);
+                Blog blog = new Blog(blogID, userID, fullname, categoryID, thumbnail, title, updatedDate, blogDetail, flag, status);
+                list.add(blog);
                 }
             }
         } catch (Exception e) {
