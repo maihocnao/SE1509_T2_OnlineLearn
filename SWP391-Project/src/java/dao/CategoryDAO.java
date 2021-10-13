07e958011d7f5cc33a27d87e34fad8c2836d10ef
QuanNC <90890786+QuanNC@users.noreply.github.com>
2021-10-13 11:03:46 +0700

    Update CategoryDAO.java

--- SWP391-Project/src/java/dao/CategoryDAO.java
+++ SWP391-Project/src/java/dao/CategoryDAO.java
@@ -7,13 +7,21 @@
  * Record of change:<br>
  * DATE          Version    Author           DESCRIPTION<br>
  * 2021-09-30    1.0        Mai           First Version<br>
+ * 2021-10-09    1.1        Quan          Second Version<br>
  */
 package dao;
+
+import bean.Category;
+import java.util.ArrayList;
 
 /**
  *
  * @author Viettech88.vn
  */
 public interface CategoryDAO {
-    
+    public ArrayList<Category> getAllCategories();
+    public void updateCategory(String type, String categoryName,int categoryID);
+    public Category getCategoryById(int id);
+    public void deleteCategory(int id);
+    public void addCategory(String type, String name);
 }
