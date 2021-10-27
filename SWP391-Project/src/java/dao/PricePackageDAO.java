558540edf11c4308b44fbcf46f054689a7caed32
QuanNC <90890786+QuanNC@users.noreply.github.com>
2021-10-27 12:37:18 +0700

    Update PricePackageDAO.java

--- SWP391-Project/src/java/dao/PricePackageDAO.java
+++ SWP391-Project/src/java/dao/PricePackageDAO.java
@@ -7,6 +7,7 @@
  * Record of change:<br>
  * DATE          Version    Author           DESCRIPTION<br>
  * 2021-09-30    1.0        Mai           First Version<br>
+ * 2021-10-27    1.1        Quan          Second Version<br>
  */
 package dao;
 import bean.PricePackage;
@@ -19,4 +20,8 @@ import java.util.ArrayList;
  */
 public interface PricePackageDAO {
     ArrayList<PricePackage> getPPbySubjectID(String subjectID) throws Exception;
+    public void addPricePackage(String name, int accessduration, String status, float listprice, float saleprice, String description, String subjectid);
+    public void editPricePackage(int pricepackageid, String name, int accessduration, String status, float listprice, float saleprice, String description, String subjectid);
+    public void deletePricePackage(int pricepackageid);
+    PricePackage getPPbyId(int pricepackageid);
 }
