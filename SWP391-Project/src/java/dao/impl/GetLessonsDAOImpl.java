bcb5476579e29ce191ea4496c188e4fffc28986c
maihocnao <85759025+maihocnao@users.noreply.github.com>
2021-10-28 14:37:14 +0700

    Update GetLessonDAOImpl

--- SWP391-Project/src/java/dao/impl/GetLessonsDAOImpl.java
+++ SWP391-Project/src/java/dao/impl/GetLessonsDAOImpl.java
@@ -11,64 +11,64 @@
 package dao.impl;
 import context.DBContext;
 import bean.Lesson;
-import bean.Subject;
 import dao.LessonDAO;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 /**
  * This class implements from class interface LessonDAO. <br>
  * This class contains method to query select data from the table
- * Information.<br>
- * There are get all Information.
+ * Lesson.<br>
+ * There are get all Information, a list of lesson by subjectID, a lesson by lessonID
  *
  * @author Mai
  */
 public class GetLessonsDAOImpl extends DBContext implements LessonDAO{
 
-    @Override
-    public void list() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-/**
+    
+    /**
      * Get a  get All Lesson
-     * @param string sid
+     * @param sid is a string
      * @return an array list of Lessons
      * @throws Exception
      */
     @Override
     public ArrayList<Lesson> getAllLessonBySID(String sid) throws Exception {
        
-       Connection conn = null;
-        PreparedStatement statement = null;
-        ResultSet rs = null;
-        ArrayList<Lesson> arr = new ArrayList<>();
+        Connection conn = null; //connection from db
+        PreparedStatement statement = null; //statement to execute and user var
+        ResultSet rs = null; // resultset to get result
+        
+        ArrayList<Lesson> arr = new ArrayList<>();//arraylist of lesson
+        //sql statement
         String sql="SELECT * from Lesson where subjectID=?";
-     //   int subjectID, int categoryID, String thumbnail, String name, String Description
+   
+        //try catch to get result from database
         try { 
+            //get connection
             conn = getConnection();
             statement = conn.prepareStatement(sql);
             statement.setString(1, sid);
+            //execute statement
             rs = statement.executeQuery();
             while(rs.next()) {
-                int lessonID = rs.getInt(1); 
-                String lessonType = rs.getString(2);
-                String lessonName = rs.getString(3);
-                String youtubeLink = rs.getString(4);
-                String content = rs.getString(5); 
-                String status = rs.getString(6);
-                String subjectID = rs.getString(7); 
-                Lesson s = new Lesson(lessonID, lessonType, lessonName, youtubeLink, content, status, subjectID);
-                arr.add(s);
+                int lessonID = rs.getInt(1); //indentation lesson id
+                String lessonType = rs.getString(2); //String type of lesson
+                String lessonName = rs.getString(3); // String Name of lesson 
+                String youtubeLink = rs.getString(4);//link youtube
+                String content = rs.getString(5); //content of lesson
+                String status = rs.getString(6); //string status of lesson
+                String subjectID = rs.getString(7); //string id of subject of lesson
+                Lesson s = new Lesson(lessonID, lessonType, lessonName, youtubeLink, content, status, subjectID);//push to object s
+                arr.add(s);//add to array
              }
-            
+         //return value of lesson array   
          return arr;
         } catch (ClassNotFoundException | SQLException ex) {
            throw ex;
+           //close rs, statement, connection
         } finally {
             closeResultSet(rs);
             closePreparedStatement(statement);
@@ -76,21 +76,20 @@ public class GetLessonsDAOImpl extends DBContext implements LessonDAO{
         }
         
     }
-    public static void main(String[] args) {
-        GetLessonsDAOImpl a = new GetLessonsDAOImpl();
-        try {
-            System.out.println(a.getALessonByID(1));
-        } catch (Exception ex) {
-            Logger.getLogger(GetLessonsDAOImpl.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-
-    @Override
+   
+    /**
+     * Get First Lesson option according in the database
+     *
+     * @param lessonID it is an int
+     * @return a list <code>Lesson</code> object
+     * @throws Exception
+     */
     public Lesson getALessonByID(int lessonID) throws Exception {
         
-        Connection conn = null;
-        PreparedStatement statement = null;
-        ResultSet rs = null;
+          
+       Connection conn = null; //connection from db
+        PreparedStatement statement = null; //statement to execute and user var
+        ResultSet rs = null; // resultset to get result
         Lesson lesson = new Lesson();
         String sql=" SELECT TOP 1 [lessonID]\n" +
 "      ,[lessonType]\n" +
@@ -106,20 +105,21 @@ public class GetLessonsDAOImpl extends DBContext implements LessonDAO{
             statement.setInt(1, lessonID);
             rs = statement.executeQuery();
             while(rs.next()) {
-                int lessonId = rs.getInt(1); 
-                String lessonType = rs.getString(2);
-                String lessonName = rs.getString(3);
-                String youtubeLink = rs.getString(4);
-                String content = rs.getString(5); 
-                String status = rs.getString(6);
-                String subjectID = rs.getString(7); 
-                lesson= new Lesson(lessonId, lessonType, lessonName, youtubeLink, content, status, subjectID);
+                int lessonId = rs.getInt(1); //indentation lesson id
+                String lessonType = rs.getString(2); //String type of lesson
+                String lessonName = rs.getString(3); // String Name of lesson 
+                String youtubeLink = rs.getString(4);//link youtube
+                String content = rs.getString(5); //content of lesson
+                String status = rs.getString(6); //string status of lesson
+                String subjectID = rs.getString(7); //string id of subject of lesson
+                lesson= new Lesson(lessonId, lessonType, lessonName, youtubeLink, content, status, subjectID); //push to object lesson
                 
              }
-            
+         //return a object lesson   
          return lesson;
         } catch (ClassNotFoundException | SQLException ex) {
            throw ex;
+           //close rs, statement, connection
         } finally {
             closeResultSet(rs);
             closePreparedStatement(statement);
@@ -127,11 +127,19 @@ public class GetLessonsDAOImpl extends DBContext implements LessonDAO{
         }
     }
 
+    /**
+     * Get  A Lesson options according in the database
+     *
+     * @param sid it is a string
+     * @return a list <code>Lesson</code> object
+     * @throws Exception
+     */
     @Override
     public Lesson getFirstLesson(String sid) throws Exception {
-        Connection conn = null;
-        PreparedStatement statement = null;
-        ResultSet rs = null;
+           
+       Connection conn = null; //connection from db
+        PreparedStatement statement = null; //statement to execute and user var
+        ResultSet rs = null; // resultset to get result
         Lesson lesson = new Lesson();
         String sql="SELECT TOP 1 [lessonID]\n" +
 "      ,[lessonType]\n" +
@@ -147,20 +155,21 @@ public class GetLessonsDAOImpl extends DBContext implements LessonDAO{
             statement.setString(1, sid);
             rs = statement.executeQuery();
             while(rs.next()) {
-                int lessonId = rs.getInt(1); 
-                String lessonType = rs.getString(2);
-                String lessonName = rs.getString(3);
-                String youtubeLink = rs.getString(4);
-                String content = rs.getString(5); 
-                String status = rs.getString(6);
-                String subjectID = rs.getString(7); 
-                lesson= new Lesson(lessonId, lessonType, lessonName, youtubeLink, content, status, subjectID);
+                int lessonId = rs.getInt(1); //indentation lesson id
+                String lessonType = rs.getString(2); //String type of lesson
+                String lessonName = rs.getString(3); // String Name of lesson 
+                String youtubeLink = rs.getString(4);//link youtube
+                String content = rs.getString(5); //content of lesson
+                String status = rs.getString(6); //string status of lesson
+                String subjectID = rs.getString(7); //string id of subject of lesson
+                lesson= new Lesson(lessonId, lessonType, lessonName, youtubeLink, content, status, subjectID); //push to object lesson
                 
              }
             
          return lesson;
         } catch (ClassNotFoundException | SQLException ex) {
            throw ex;
+           //close rs, statement, connection
         } finally {
             closeResultSet(rs);
             closePreparedStatement(statement);
