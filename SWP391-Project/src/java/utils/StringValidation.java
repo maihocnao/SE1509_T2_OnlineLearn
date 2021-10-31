b2e8bd4373911a622e48ad582117ad4f96399592
maihocnao <85759025+maihocnao@users.noreply.github.com>
2021-10-31 12:39:39 +0700

    User Authorization

---
+++ SWP391-Project/src/java/utils/StringValidation.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (C) 2021, TEAM 2 ONLINE LEARN
+ *
+ * SWP_OnlineLearn
+ * SWP ONLINE LEARN PROJECT - TEAM 2
+ *
+ * Record of change:<br>
+ * DATE          Version    Author           DESCRIPTION<br>
+ * 2021-09-30    1.0        Mai           First Version<br>
+ */
+package utils;
+
+/**
+ *
+ * @author cuong
+ */
+public class StringValidation {
+
+    public boolean isNullOrEmpty(String data) {
+        return data == null || data.isEmpty();
+    }
+}
